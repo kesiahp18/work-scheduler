b4dc74f9c13a5e37e772f2eeb5a764ee196a7c45
Kesiah Patil <kesiahpatil@Kesiahs-MacBook-Pro.local>
2020-11-15 16:58:49 -0500

    added date to page header

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,15 +18,16 @@ $(document).ready(function(){
     } else {
       JSON.parse(localStorage.getItem('timeBlocks'));
     }
-  })
+    //set date to current day at top of page
+    $('#currentDay').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY'));
+
+})
 
 //save inputted content to local storage when save button is clicked
 $("#save").click(function (event) {
     event.preventDefault();
-
     var hour = parseInt($(this).siblings("div").text().trim());
     var hourContent = $(this).siblings("textarea").val().trim();
     console.log(hour);
     console.log(hourContent);
-
 });
