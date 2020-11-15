96f6d0347b33d002a74d91b3ae992bfd959f3ed9
Kesiah Patil <kesiahpatil@Kesiahs-MacBook-Pro.local>
2020-11-15 14:58:43 -0500

    added time blocks and initialized to local storage

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var timeBlocks = {
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": ""
+}
+
+$(document).ready(function(){
+    //if there is nothing saved in the timeblocks
+    if(!localStorage.getItem('timeBlocks')) {
+        //initialize the empty time blocks to local storage
+        localStorage.setItem('timeBlocks', JSON.stringify(timeBlocks));
+    } else {
+      JSON.parse(localStorage.getItem('timeBlocks'));
+    }
+  })
+
+//save inputted content to local storage when save button is clicked
+$("#save").click(function (event) {
+    event.preventDefault();
+
+    var hour = parseInt($(this).siblings("div").text().trim());
+    var hourContent = $(this).siblings("textarea").val().trim();
+    console.log(hour);
+    console.log(hourContent);
+
+});
