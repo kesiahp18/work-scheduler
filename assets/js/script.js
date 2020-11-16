0974bc838d895e6e33d59163bdedb1326700e542
Kesiah Patil <kesiahpatil@Kesiahs-MacBook-Pro.local>
2020-11-15 22:22:39 -0500

    hour content loads from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,49 @@ var timeBlocks = {
     "4 PM": "",
     "5 PM": ""
 }
+var presentHour = moment().hour();
 
 $(document).ready(function(){
     //if there is nothing saved in the timeblocks
     if(!localStorage.getItem('timeBlocks')) {
-        //initialize the empty time blocks to local storage
-        localStorage.setItem('timeBlocks', JSON.stringify(timeBlocks));
+      saveTasks(timeBlocks);
     } else {
-      JSON.parse(localStorage.getItem('timeBlocks'));
+      saveTasks(JSON.parse(localStorage.getItem('timeBlocks')));
     }
     //set date to current day at top of page
     $('#currentDay').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY'));
-
-})
-
-//save inputted content to local storage when save button is clicked
-$("#save").click(function (event) {
-    event.preventDefault();
-    var hour = parseInt($(this).siblings("div").text().trim());
-    var hourContent = $(this).siblings("textarea").val().trim();
-    console.log(hour);
-    console.log(hourContent);
-});
+  })
+  //when a save button is clicked
+  $(".saveBtn").click(function(event) {
+    event.preventDefault()
+    //get the user input from the textarea and save it with the hour
+    value = $(this).siblings("textarea").val();
+    schedule = $(this).siblings("div").text();
+    save(schedule, value);
+  });
+  
+  //loads time blocks from local storage
+  function loadTimeBlocks() {
+    result = localStorage.getItem('timeBlocks')
+    return (timeBlocks);
+  }
+  //saves 
+  function saveToLocalStorage(dayData) {
+    localStorage.setItem('timeBlocks', JSON.stringify(dayData));
+  }
+  //initializes local storage if nothing has been saved yet
+  function save(schedule, val) {
+    if(!localStorage.getItem('timeBlocks')) {
+        localStorage.setItem('timeBlocks', JSON.stringify(timeBlocks));
+    }
+    let workHours = JSON.parse(localStorage.getItem('timeBlocks'));
+    workHours[schedule] = val
+    saveToLocalStorage(workHours);
+  }
+  //
+  function saveTasks(dayObject) {
+    $(".calendar-row").each(function(index) {
+      let res = $(this).children("div");
+      $(this).children("textarea").text(dayObject[res.text()]);
+    })
+  }
